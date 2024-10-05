14df4746d5e506d9df520e82ccab9ee563116f33
Lukiatus <lukiatus@example.com>
2024-10-05 14:03:59 +0200

    Migrate .eslintrc.json to the new ESLint configuration file format

--- jam-time-frontend/eslint.config.mjs
+++ jam-time-frontend/eslint.config.mjs
@@ -1,11 +1,88 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import angularEslint from "@angular-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
 
-export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-];
+export default [{
+    ignores: ["projects/**/*"],
+}, ...compat.extends(
+    "plugin:@angular-eslint/recommended",
+    "plugin:@angular-eslint/template/process-inline-templates",
+).map(config => ({
+    ...config,
+    files: ["**/*.ts"],
+})), {
+    files: ["**/*.ts"],
+
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+        "@angular-eslint": angularEslint,
+    },
+
+    languageOptions: {
+        parser: tsParser,
+    },
+
+    rules: {
+        "@typescript-eslint/explicit-function-return-type": "error",
+        "@typescript-eslint/explicit-member-accessibility": "error",
+        "@typescript-eslint/no-explicit-any": "error",
+
+        "@typescript-eslint/member-ordering": ["error", {
+            classes: [
+                "private-field",
+                "protected-field",
+                "public-field",
+                "public-get",
+                "protected-get",
+                "private-get",
+                "public-set",
+                "protected-set",
+                "private-set",
+                "constructor",
+                "public-method",
+                "protected-method",
+                "private-method",
+            ],
+        }],
+
+        "@angular-eslint/directive-selector": ["error", {
+            type: "attribute",
+            prefix: "app",
+            style: "camelCase",
+        }],
+
+        "@angular-eslint/component-selector": ["error", {
+            type: "element",
+            prefix: "app",
+            style: "kebab-case",
+        }],
+
+        "no-var": "error",
+        curly: "error",
+        "no-console": "error",
+        "max-lines": ["warn", 150],
+    },
+}, ...compat.extends("plugin:@angular-eslint/template/recommended").map(config => ({
+    ...config,
+    files: ["**/*.html"],
+})), {
+    files: ["**/*.html"],
+
+    rules: {
+        "@angular-eslint/template/no-any": ["error"],
+        "@angular-eslint/template/banana-in-box": ["error"],
+        "@angular-eslint/template/no-inline-styles": ["error"],
+    },
+}];
